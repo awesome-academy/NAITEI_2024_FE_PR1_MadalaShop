f7671f213764c8da3b62b5c277655c4fc434e28a
NAM <nam@NAMs-MacBook-Pro-3.local>
2024-08-14 18:06:54 +0700

    79110

--- asset/scripts/navbar.js
+++ asset/scripts/navbar.js
@@ -2,7 +2,7 @@ fetch('navbar.html')
 .then(response => response.text())
 .then(data => {
     document.getElementById('content').innerHTML = data;
-    const newStyleSheet = document.createElement('link');
+    const newStyleSheet = document.createElement("link");
                 newStyleSheet.rel = 'stylesheet';
                 newStyleSheet.href = '../css/navbar.css';
                 document.head.appendChild(newStyleSheet);
